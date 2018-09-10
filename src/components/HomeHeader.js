87847234a2f7d6698daae2926a82fd0fba04895a
Chris Lucas <chris@hhd.com.au>
2018-09-10 20:47:24 +1000

    Check if on homepage and render homepage banner

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HomeHeader = () => (
+  <header>Hello</header>
+)
+
+export default HomeHeader
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,20 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Sticky from 'react-sticky-el';
+import { withPrefix } from "gatsby-link";
 
 import Navbar from '../components/Navbar'
+import HomeHeader from '../components/HomeHeader'
 import '../stylesheets/all.scss'
 import 'prismjs/themes/prism-tomorrow.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Sticky className="sticky-nav">
-      <Navbar />
-    </Sticky>
-    <div>{children()}</div>
-  </div>
-)
+const TemplateWrapper = ({ children }) => {
+  const isHomepage = location.pathname === withPrefix("/");
+  return (
+    <div>
+      <Helmet title="𝖈𝖍𝖗𝖎𝖘𝖈𝖗𝖊𝖆𝖙.𝖊𝖘" />
+      { isHomepage && <HomeHeader />}
+      <Sticky className="sticky-nav">
+        <Navbar />
+      </Sticky>
+      <div>{children()}</div>
+    </div>
+  )
+  }
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
