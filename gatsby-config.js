84a06b9f9126ded0213ab2e2cfe22c9884735669
Chris Lucas <chris@hhd.com.au>
2019-02-12 07:42:02 +1100

    Added site manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,60 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "Chris Creates"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [],
-      },
+        plugins: []
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-prismjs`,
-          },
-        ],
-      },
+            resolve: `gatsby-remark-prismjs`
+          }
+        ]
+      }
     },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Chris Creates",
+        short_name: "chriscreat.es",
+        start_url: "/",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+        display: "standalone"
+      }
+    },
+    // "gatsby-plugin-offline",
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
+};
